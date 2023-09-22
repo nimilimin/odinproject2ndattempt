4db3e37e8e9c23a26e513663fe55545eb1fd11db
Nimil <nimilgeorgepradeepngp@gmail.com>
2023-09-22 16:53:09 +0530

    tried out functions

---
+++ 3func.js
@@ -0,0 +1,17 @@
+function add7(num){
+	console.log(num + 7);
+}
+
+function mult2nums(num1,num2){
+	console.log(num1*num2);
+}
+
+function capitilize(theString){
+	console.log(theString[0].toUpperCase() + theString.slice(1));
+}
+
+add7(5);
+
+mult2nums(10,20);
+
+capitilize("hELlo, worLd!!");
